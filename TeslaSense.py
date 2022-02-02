e023dfa15da43fbb8cfc48a21345b6befdbab5d7
israndy <israndy@yahoo.com>
2022-02-02 15:33:03 -0800

    Update TeslaSense.py

--- TeslaSense.py
+++ TeslaSense.py
@@ -1,9 +1,7 @@
-#import re
-#from datetime import datetime
-#from datetime import date
-#from datetime import time
 from time import sleep
 
+# To install support module:
+# Python3 -m pip install sense_energy
 print ("Initating connection to Sense...")
 from sense_energy import Senseable
 username = 'elon@tesla.com'
@@ -11,6 +9,7 @@ password = 'password'
 sense = Senseable(wss_timeout=30,api_timeout=30)
 sense.authenticate(username, password)
 
+# Python3 -m pip install teslapy
 print ("Starting connection to Tesla...")
 import teslapy
 with teslapy.Tesla('elon@tesla.com') as tesla:
@@ -24,9 +23,8 @@ with teslapy.Tesla('elon@tesla.com') as tesla:
     except:
         print("Vehicle may be sleeping\n")
         charging = False
-
-rate = 5            # Minimum rate charger can go to
-threshold = 600     # Minimum watts needed to start charging
+        
+amps = 5            # Minimum rate charger can go to
 volts = 120         # Minimum volts in those watts
 
 while (True):
@@ -40,42 +38,37 @@ while (True):
     try :
         if vehicles[0].get_vehicle_data()['charge_state']['charging_state'] == "Charging":
             charging = True
+            amps = vehicles[0].get_vehicle_data()['charge_state']['charge_current_request']
             maxamps = vehicles[0].get_vehicle_data()['charge_state']['charge_current_request_max']
-            rate = vehicles[0].get_vehicle_data()['charge_state']['charge_current_request']
             volts = vehicles[0].get_vehicle_data()['charge_state']['charger_voltage']
-            threshold = rate * volts
         else:
             charging = False
     except :
         charging = False
 
-    newrate = rate + int( power_diff / volts )
-
     if charging :                           # check if need to change rate or stop
-        if (power_diff > 0) :
-            print ("Charging at", rate, "amps, with", power_diff, "watts surplus")
-            if newrate > maxamps :
-                newrate = maxamps
-            if newrate < 5 :                                # minimum charge rate
-                    newrate = 5
-            if newrate > rate :
+        newrate = amps + int( power_diff / volts )
+        if newrate > maxamps :
+            newrate = maxamps
+        if power_diff > 0 :
+            print ("Charging at", amps, "amps, with", power_diff, "watts surplus")
+            if newrate > amps :
                 print ("Increasing charging to", newrate, "amps")
                 vehicles[0].command('CHARGING_AMPS', charging_amps = newrate )
-                rate = newrate
-            elif rate < newrate :
-                print ("Slowing charging to", newrate, "amps")
-                vehicles[0].command('CHARGING_AMPS', charging_amps = newrate )
-                rate = newrate
+                amps = newrate
         else :                                                 # Not enough power
-            print ("Charging at", rate, "amps, with", power_diff, "watts to spare")
-            if ((newrate - rate ) * volts ) < power_diff :
+            print ("Charging at", amps, "amps, with", power_diff, "usaage")
+            if newrate < 5 :                            # can't charge below 5 amps
                 print ("Stopping charge")
                 vehicles[0].command('STOP_CHARGE')
                 charging = False
-
-    else :                                # NOT Charging, check if need to start
-        print ("Not Charging, Spare power at", power_diff)
-        if power_diff > (5 * volts) :                           # Minimum charge rate
+            elif newrate < amps :
+                print ("Slowing charging to", newrate, "amps")
+                vehicles[0].command('CHARGING_AMPS', charging_amps = newrate )
+                amps = newrate
+    else :                                  # NOT Charging, check if need to start
+        print ("Not Charging, Spare power at", power_diff, "watts")
+        if power_diff > (5 * volts) :                         # Minimum charge rate
             try :
                 print ("Starting charge")
                 vehicles[0].sync_wake_up()
@@ -87,11 +80,11 @@ while (True):
                 charging = True
             except :
                 print ("failed to start charging")
+                charging = False
             try :
                 vehicles[0].command('CHARGING_AMPS', charging_amps=5)
             except :
                 print ("Failed to set rate")
-                charging = False
 
     print("Waiting 60 sec...")
     sleep(60) #The fastest the Sense API will update
