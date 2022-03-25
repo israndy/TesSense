762ace29abd3116c51e24f2a3af2df6135a8d319
israndy <israndy@yahoo.com>
2022-03-25 09:20:23 -0700

    Update TesSense.py

--- TesSense.py
+++ TesSense.py
@@ -1,6 +1,4 @@
-import asyncio, sys, logging
-
-import datetime
+import datetime, sys, logging
 from time import sleep
 
 # To install support module:
@@ -8,7 +6,10 @@ from time import sleep
 from sense_energy import Senseable
 
 # Python3 -m pip install senselink
+import asyncio
 from senselink import SenseLink
+from senselink.plug_instance import PlugInstance
+from senselink.data_source import MutableSource
 
 # Python3 -m pip install teslapy
 import teslapy
@@ -17,12 +18,14 @@ username = 'elon@tesla.com'         # Sense's and Tesla's login
 password = 'password'               # Sense's password, Tesla will prompt for it's own
 
 """
- TesSense w/ AsyncIO  -Randy Spencer 2022 Version 5
- Python charge monitoring utility for those who own the Sense Energy Monitor.
+ TesSense w/ SenseLink  -Randy Spencer 2022 Version 6
+ Python charge monitoring utility for those who own the Sense Energy Monitor
  Uses the stats for Production and Utilization of electricity to control
  your main Tesla's AC charging to use excess production only when charging.
+ With SenseLink integration the Tesla usage will show in the Sense app.
  Simply plug in your car, update your info above, and type> python3 tessense.py
 
+ Added: another minute to remove powering off due to spurius spikes/clouds
  Added: reporting the Tesla's charging to Sense as if plugged into a TP-Link/Kasa
  Add: read & display the watts usage of the other EV charging from the HS-110
  Add: ability to find TP-Link devices on the network and control them.
@@ -34,27 +37,29 @@ sense.authenticate(username, password)
 
 #/c Set stdout as logging handler
 root_log = logging.getLogger()
-root_log.setLevel(logging.INFO)
+root_log.setLevel(logging.WARNING)
 handler = logging.StreamHandler(sys.stdout)
 
 def printmsg(msg) :                                        # Timestamped message
     print( " ", datetime.datetime.now().strftime( "%a %I:%M %p" ), msg )
     
 def printerror(error, err) :                               # Error message with truncated data
+    print("Error "*20)
     print(str(err).split("}")[0], "}\n", datetime.datetime.now().strftime( "%a %I:%M %p" ), error)
 
 
 def UpdateSense() :                                        # Update Sense info
-    global power_diff
+    global power_diff, volts
     try :
         sense.update_realtime()
     except :
         printmsg("Sense Timeout")
         return(True)
     else :
+        volts = int(sense.active_voltage[0] + sense.active_voltage[1])
         asp = int(sense.active_solar_power)
         ap = int(sense.active_power)
-        power_diff = asp-ap                                # Watts being set back to the grid
+        power_diff = asp-ap                                # Watts being sent back to the grid
 
 def UpdateTesla(car) :                                     # Grab the latest charge info from the Tesla
     global chargedata
@@ -64,75 +69,59 @@ def UpdateTesla(car) :                                     # Grab the latest cha
         printerror("Tesla failed to update, please wait a minute...", e)
         return(True)
 
-def PrintUpdate(fast) :
-    global batlvl
-    if batlvl != chargedata['battery_level'] : # Display stats at every % change
-        batlvl = chargedata['battery_level']
-        print( "\nLevel:",
-            chargedata['battery_level'], "%, Limit",
-            chargedata['charge_limit_soc'], "%, Rate is",
-            chargedata['charge_amps'], "of a possible",
-            chargedata['charge_current_request_max'], "Amps,",
-            chargedata['charger_voltage'], "Volts, ",
-            chargedata['charge_energy_added'], "kWh added, ", end="")
-        if not fast : print(chargedata['time_to_full_charge'], "Hours remaining\n" )
-        else : print(chargedata['minutes_to_full_charge'], "Minutes remaining\n" )
+def PrintUpdate(fast) :                                    # Display stats at every % change
+    print( "\nLevel:",
+        chargedata['battery_level'], "%, Limit",
+        chargedata['charge_limit_soc'], "%, Rate is",
+        chargedata['charge_amps'], "of a possible",
+        chargedata['charge_current_request_max'], "Amps,\n",
+        volts, "Volts, ",
+        chargedata['charge_energy_added'], "kWh added, ", end="")
+    if not fast : print(
+        chargedata['time_to_full_charge'], "Hours remaining\n" )
+    else : print(
+        chargedata['minutes_to_full_charge'], "Minutes remaining\n" )
 
 def CheckSuperCharging() :
     if chargedata['fast_charger_present']:                 # Loop while DC Fast Charging
         printmsg("Supercharging...")
-        PrintUpdate(True)
+        PrintUpdate(1)
         return(True)
 
-def UpdateACcharging() :                                   # AC Charging so collect charging info
-    global volts, rate, maxrate, newrate, mutable_plug
-    volts =   chargedata['charger_voltage']
-    rate =    chargedata['charge_current_request']
-    maxrate = chargedata['charge_current_request_max']
-    newrate = min( rate + int( power_diff / volts ), maxrate )
-    mutable_plug.data_source.power = rate * volts      # For times when rate changes outside this app
-    mutable_plug.data_source.voltage = volts
-    if -2 < newrate < minrate : newrate = 0                  # Deadzone where charger is on at zero amps
-        
 def SendCmd(car, cmd, err) :                               # Start or Stop charging
     try :
         car.command(cmd)
-    except teslapy.VehicleError as e : printerror(err, e)
-    except teslapy.HTTPError as e: printerror(err, e)
+    except teslapy.VehicleError as e : printerror("V: "+err, e)
+    except teslapy.HTTPError as e: printerror("H: "+err, e)
 
-def SetAmps(car, amps, err) :                              # Increase or decrease charging rate
+def SetAmps(car, newrate, err) :                              # Increase or decrease charging rate
     try :
-        car.command('CHARGING_AMPS', charging_amps = amps)
-    except teslapy.VehicleError as e : printerror(err, e)
-    except teslapy.HTTPError as e: printerror(err, e)
+        car.command('CHARGING_AMPS', charging_amps = newrate)
+    except teslapy.VehicleError as e : printerror("V: "+err, e)
+    except teslapy.HTTPError as e: printerror("H: "+err, e)
 
-def ChangeCharging(car, msg) :
+def ChangeCharging(car, newrate, msg) :
     print( msg, "charging to", newrate, "amps" )
     SetAmps(car, newrate, "Failed to change")
     if newrate < 5 : SetAmps(car, newrate, "Failed to change 2")
-    rate = newrate
-    mutable_plug.data_source.power = rate * volts
 
 def StopCharging(car) :
     print( "Stopping charge" )
     SendCmd(car, 'STOP_CHARGE', "Failed to stop")
-    mutable_plug.data_source.power = rate * volts
 
-def StartCharging(car) :
-    global newrate, rate, volts
+def StartCharging(car, newrate) :
     if chargedata['charging_state'] == "Disconnected":
         printmsg("Please plug in")
-        return(True)
+        return(2)
     elif chargedata['battery_level'] >= chargedata['charge_limit_soc'] :
         printmsg("Full Battery")
-        return(True)
+        return(1)
     elif chargedata['charging_state'] != "Charging" :
-        print( "Starting charge")
+        print( "Starting charge at", newrate, "Amps")
         SendCmd(car, 'START_CHARGE', "Won't start charging")
         SetAmps(car, newrate, "Won't start charging 2")        # Set the Amps twice for values under 5 amps
         if newrate < 5 : SetAmps(car, newrate, "Won't start charging 3")
-        rate = newrate
-        mutable_plug.data_source.power = rate * volts
+        return(0)
         
 def Wake(car) :
     print("Waking...")
@@ -140,72 +129,91 @@ def Wake(car) :
     except teslapy.VehicleError as e : printerror("Won't wake up", e)
     except teslapy.HTTPError as e: printerror("Failed to wake", e)
 
-async def run_tes_sense(mutable_plug):
-    global batlvl, rate, newrate, minrate, maxrate, volts
-    batlvl = rate = minrate = 3                                # Minimum rate you want to set the charger to
-    volts = 120                                                # Minimum volts, until detected by the Tesla
+async def run_tessense(mutable_plug):
+    rate = newrate = level = limit = fullORunplugged = 0
+    minrate = 3                                            # Minimum rate you want to set the charger to
 
-    with teslapy.Tesla(username) as tesla:
+    retry = teslapy.Retry(total=3, status_forcelist=(500, 502, 503, 504))
+    with teslapy.Tesla(username, retry=retry, timeout=20) as tesla:
         vehicles = tesla.vehicle_list()
-        car = vehicles[0].get_vehicle_summary()
-        print("Starting connection to", car['display_name']+"...\n")
-            
+        print("Starting connection to", vehicles[0].get_vehicle_summary()['display_name']+"...\n")
+        
         while (True):
             if UpdateSense() :                             # Collect new data from Energy Monitor
-                await asyncio.sleep(20)
+                await asyncio.sleep(20)                    # Error: Return to top of order
                 continue
+                
             if vehicles[0].available() :                   # Check if car is online
                 if UpdateTesla(vehicles[0]) :              # Collect new data from Tesla
-                    await asyncio.sleep(60)
+                    await asyncio.sleep(60)                # Error: Return to top of order
                     continue
-                if CheckSuperCharging() :                  # Display any Supercharging or DCFC data
-                    await asyncio.sleep(120)
+                elif CheckSuperCharging() :                # Display any Supercharging or DCFC data
+                    await asyncio.sleep(120)               # Loop while Supercharging back to top
                     continue
-                UpdateACcharging()                         # Collect AC Charging data from Tesla
-                if chargedata['charging_state'] == "Charging" :# Check if need to change rate or stop
-                    PrintUpdate(False)
+                else :                                     # Calculate new rate of charge
+                    rate = chargedata['charge_current_request']
+                    maxrate = chargedata['charge_current_request_max']
+                    newrate = min( rate + int( power_diff / volts ), maxrate )
+                    if -5 < newrate < minrate : newrate = 0 # Deadzone where charger is on at zero amps
+                                        
+                if chargedata['charging_state'] == "Charging" : # Display where we have been
+                    if  level != chargedata['battery_level'] or limit != chargedata['charge_limit_soc'] :
+                        level, limit = chargedata['battery_level'], chargedata['charge_limit_soc']
+                        PrintUpdate(0)                     # Display charging info every % change
                     if power_diff > 1 :                    # Enough free power to continue
                         print( "Charging at", rate, "amps, with", power_diff, "watts surplus" )
                     elif power_diff < -1 :                 # Not enough free power to continue
                         print( "Charging at", rate, "amps, with", power_diff, "watts usage" )
                     else : print( "Charging at", rate, "amps" ) # power_diff = -1, 0 or 1, so don't say "watts"
-                    if newrate < 0 :                       # Stop charging if there's no free power
+                                                           # Charging so see if need to stop or change rate
+                    if newrate < 0 :                       # Stop charging as there's no free power
                         StopCharging(vehicles[0])
                     elif newrate > rate :                  # Charge faster with any surplus
-                        ChangeCharging(vehicles[0], "Increasing")
+                        ChangeCharging(vehicles[0], newrate, "Increasing")
                     elif newrate < rate :                  # Charge slower due to less availablity
-                        ChangeCharging(vehicles[0], "Slowing")
-                    else :
-                        mutable_plug.data_source.power = rate * volts
+                        ChangeCharging(vehicles[0], newrate, "Slowing")
+                    rate = newrate
+                    mutable_plug.data_source.power = rate * volts # Update Sense with current info (Ha!)
+                    
                 else :                                     # Not charging, check if need to start
+                    mutable_plug.data_source.power = 0     # No longer charging, let Sense know
                     print( "Not Charging, Spare power at", power_diff, "watts" )
-                    if power_diff > ( minrate * volts ) :  # Minimum free watts to wake car and charge
-                        if StartCharging(vehicles[0]) :
-                            await asyncio.sleep(180)
+                    if power_diff > ( minrate * volts ) :  # Minimum free watts to charge car
+                        fullORunplugged = StartCharging(vehicles[0], newrate)
+                        if fullORunplugged :
+                            await asyncio.sleep(180)       # Didn't start so cycle back to top
                             continue
             else :                                         # Sleeping, check if need to wake and charge
-                print("Sleeping, free power is", power_diff, "watts" )
-                if power_diff > ( minrate * volts ) :
-                    Wake(vehicles[0])
+                if power_diff > ( minrate * volts ) :      # There is enough free solar to charge with
+                    Wake(vehicles[0])                      # Also an initial wake() to get vehicle status
+                    rate = newrate = 0                     # Reset rate as things will have changed
                     continue
-            printmsg(" Wait two minutes...")               # Message every loop
+                elif fullORunplugged == 1 :                # Because battery is at the charge limit
+                    print("Battery full to limit, free power is", power_diff, "watts" )
+                elif fullORunplugged == 2 :                # Because Tesla is not plugged in
+                    print("Please plug in, free power is", power_diff, "watts" )
+                else : print("Sleeping, free power is", power_diff, "watts" ) # Just not enough to charge
+
+            printmsg(" Wait two minutes...")               # Message after every complete loop
             await asyncio.sleep(120)                       # Fastest the Sense API will update is 30 sec.
+            
 
-
-async def main():
+async def main():                                          # Much thanks to cbpowell for this SenseLink code:
     global mutable_plug
-    # Get config
-    config = open('config.yml', 'r')
-    # Create controller, with config
-    controller = SenseLink(config)
-    # Create instances
-    controller.create_instances()
-
-    # Get Mutable controller object, and create task to update it
-    mutable_plug = controller.plug_for_mac("53:75:31:f8:3a:8c")
+    # Create controller, with NO config
+    controller =  SenseLink(None)
+    
+    # Create a PlugInstance, setting at least the identifier and MAC
+    mutable_plug = PlugInstance("mutable", alias="Tesla", mac="53:75:31:f8:3a:8c")
+    # Create and assign a Mutable Data Source to that plug
+    mutable_data_source = MutableSource("mutable", None)
+    mutable_plug.data_source = mutable_data_source
+    
+    # Add that plug to the controller
+    controller.add_instances({mutable_plug.identifier: mutable_plug})
 
     # Pass plug to TesSense, where TesSense can update it
-    tes_task = run_tes_sense(mutable_plug)
+    tes_task = run_tessense(mutable_plug)
 
     # Get SenseLink tasks to add these
     tasks = controller.tasks
